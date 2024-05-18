a4542cbda989a45b7505a07cea9ff2d4bcdebe77
kkhs2 <shumkhk@gmail.com>
2024-05-18 21:42:42 +0100

    complete rewrite of the components.js file

--- datasource.js
+++ datasource.js
@@ -1,4 +1,4 @@
-export const productsData = [
+const productsData = [
   {
     title: 'Coca-Cola Zero Sugar',
     volume: '24 x 330ml',
@@ -6,7 +6,7 @@ export const productsData = [
     midascode: 100001,
     brand: 'coca-cola',
     price: 13.09,
-    rrp: 1.30,
+    rrp: 1.39,
     por: 49.7
   },
   {
@@ -23,7 +23,7 @@ export const productsData = [
     title: 'Coca-Cola Original',
     volume: '24 x 250ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/13869129-4134964590200453.jpg',
-    midascode: 100003,
+    midascode: 100002,
     brand: 'coca-cola',
     price: 13.99,
     rrp: 1.39,
@@ -33,7 +33,7 @@ export const productsData = [
     title: 'Coca-Cola Cherry',
     volume: '24 x 330ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657920-1654899330330921.jpg',
-    midascode: 100004,
+    midascode: 100002,
     brand: 'coca-cola',
     price: 13.99,
     rrp: 1.39,
@@ -43,7 +43,7 @@ export const productsData = [
     title: 'Coca-Cola Original Taste Glass Bottles',
     volume: '24 x 330ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657918-9414807812038433.jpg',
-    midascode: 100005,
+    midascode: 100002,
     brand: 'coca-cola',
     price: 13.99,
     rrp: 1.39,
@@ -53,7 +53,7 @@ export const productsData = [
     title: 'Coca-Cola Zero Sugar Lemon',
     volume: '24 x 330ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/15227577-1255125050706956.jpg',
-    midascode: 100006,
+    midascode: 100003,
     brand: 'coca-cola',
     price: 14.99,
     rrp: 1.49,
@@ -63,7 +63,7 @@ export const productsData = [
     title: 'Coca-Cola Zero Sugar Cherry',
     volume: '24 x 330ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12912901-1414889184313857.jpg',
-    midascode: 100007,
+    midascode: 100004,
     brand: 'coca-cola',
     price: 15.99,
     rrp: 1.59,
@@ -73,7 +73,7 @@ export const productsData = [
     title: 'Costa Coffee Latte',
     volume: '12 x 250ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657928-1344965615047130.jpg',
-    midascode: 100008,
+    midascode: 100005,
     brand: 'costa coffee',
     price: 15.99,
     rrp: 1.59,
@@ -83,7 +83,7 @@ export const productsData = [
     title: 'Costa Coffee Vanilla Latte',
     volume: '12 x 250ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12882462-1564965615215088.jpg',
-    midascode: 100009,
+    midascode: 100006,
     brand: 'costa coffee',
     price: 15.99,
     rrp: 1.59,
@@ -93,7 +93,7 @@ export const productsData = [
     title: 'Costa Coffee Caramel Latte',
     volume: '12 x 250ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657927-1074965615454608.jpg',
-    midascode: 100010,
+    midascode: 100006,
     brand: 'costa coffee',
     price: 15.99,
     rrp: 1.59,
@@ -103,7 +103,7 @@ export const productsData = [
     title: 'Costa Coffee Flat White',
     volume: '12 x 250ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12882463-3574965914586723.jpg',
-    midascode: 100011,
+    midascode: 100006,
     brand: 'costa coffee',
     price: 15.99,
     rrp: 1.59,
@@ -113,7 +113,7 @@ export const productsData = [
     title: 'Jack Daniel\'s and Coca-Cola',
     volume: '12 x 330ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/14509056-2415036628891027.jpg',
-    midascode: 100012,
+    midascode: 100006,
     brand: 'jack daniels',
     price: 15.99,
     rrp: 1.59,
@@ -123,10 +123,23 @@ export const productsData = [
     title: 'Jack Daniel\'s and Coca-Cola Zero',
     volume: '12 x 330ml',
     image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/14509057-9895036629017087.jpg',
-    midascode: 100013,
+    midascode: 100006,
     brand: 'jack daniels',
     price: 15.99,
     rrp: 1.59,
     por: 58.5
-  }
+  },
+];
+
+
+
+const productCategories = [
+  'Coca Cola Original Taste - Glass Bottles',
+  'Fanta Orange Zero',
+  'Diet Coke',
+  'Monster Energy Drink Ultra',
+  'Relentless Fruit Punch Energy Drink',
+  'Powerade Golden Mango',
+  'Coca Cola Lemon',
+  'Reign Razzle Berry'
 ];
